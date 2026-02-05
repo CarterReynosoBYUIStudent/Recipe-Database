975b3b08c8f140113fd06103250703574e9ff037
CarterReynosoBYUIStudent <rey21032@byui.edu>
2026-02-05 15:13:21 -0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 #This is the main
+import pandas as pd
+from sqlalchemy import create_engine, text
+# Create a connection to the database
+USERNAEME = "postgres"
+PASSWORD = "password"
+DATABASE_NAME = "recipedb"
+HOST = "localhost"
+PORT = "5432"
+
+engine = create_engine(f"postgresql://{USERNAEME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE_NAME}")
+
+with engine.connect() as connection:
+    # Execute a simple query to test the connection
+    result = connection.execute(text("SELECT version();"))
+    version = result.fetchone()
+    print(f"Connected to database: {version[0]}")
+
+with engine.connect() as connection:
+    # Execute a simple query to test the connection
+    result = connection.execute(text("SELECT * FROM meals;"))
+    meals = result.fetchall()
+    for meal in meals:
+        print(meal)
